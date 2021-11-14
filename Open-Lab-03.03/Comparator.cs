8fb727316547d70ea536abab69bb2f1148304e4d
AdamHaluska <adam.haluska11@gmail.com>
2021-11-14 21:13:13 +0100

    hotovo!

--- Open-Lab-03.03/Comparator.cs
+++ Open-Lab-03.03/Comparator.cs
@@ -4,9 +4,12 @@ namespace Open_Lab_03._03
 {
     public class Comparator
     {
-        public bool CompareCharactersCount(string str1, string str2)
+         if (str1.Length == str2.Length)
+                return true;
+           
         {
-            throw new NotImplementedException();
+            else
+                return false;
         }
     }
 }
